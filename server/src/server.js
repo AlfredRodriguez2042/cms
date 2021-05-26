12384772a4c91c9d59b91c73fc18d3531fc6461e
alfred Rodriguez G <alfred2042@hotmail.com>
2021-05-26 01:48:38 -0300

    modifi server

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ const app = express()
 const path = '/graphql'
 const corsOptions = {
   credentials: true,
-  origin: ['http://localhost:5000', 'http://client:5000'],
+  origin: ['http://localhost:5000', 'http://client:5000', 'http://nginx:80'],
 }
 
 // redirects should be ideally setup in reverse proxy like nignx
